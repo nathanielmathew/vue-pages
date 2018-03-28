30c03758e990352357eada73adff2213ad309893
Nathaniel Ryan Mathew <nathanielmat2@gmail.com>
2018-03-28 12:00:56 +0530

    Update script.js
    changed start button to pause button when countdown taking place
    changed stop to reset button

--- javascript/script.js
+++ javascript/script.js
@@ -3,39 +3,69 @@ var h;
 var m;
 var s;
 
-/*function update()
-{
-setInterval(function(){now=now+1;},1000);
-}*/
 
 function start()
-{	
-	var timer = setInterval(function()										/*Timer to update information and perform operations every 1 second*/
-	{
-		acceptTime();
-		document.getElementById("SOSC").innerHTML = "Counting down...";
-		if(difference==1)
+{	if(document.getElementById("start").innerHTML=="START")
 		{
-			clearInterval(timer);										  /*To Stop Timer when countdown is complete*/
-		     
-		}
-		if(difference!=0)
-		difference--;
-		h = Math.floor(difference/(60*60));                  			  /*To Round Hour to lowest integer value*/
-		m = Math.floor((difference/60-h*60));							  /*To Round Minute to lowest integer value*/
-		s = Math.floor((difference-h*60*60-m*60));						  /*To Find Seconds Remaining*/
+			document.getElementById("start").innerHTML="PAUSE";
+			difference=1;
+			var timer = setInterval(function()										/*Timer to update information and perform operations every 1 second*/
+			{
+				
+				document.getElementById("SOSC").innerHTML = difference;//"Counting down...";
+				
+				if(difference>0)
+				{
+				acceptTime();
+				difference--;
+				h = Math.floor(difference/(60*60));                  			  /*To Round Hour to lowest integer value*/
+				m = Math.floor((difference/60-h*60));							  /*To Round Minute to lowest integer value*/
+				s = Math.floor((difference-h*60*60-m*60));						  /*To Find Seconds Remaining*/
 
-		document.getElementById("hour").value = h;
-		document.getElementById("minute").value = m;
-		document.getElementById("second").value = s;		
-	},1000);
+				document.getElementById("hour").value = h;
+				document.getElementById("hour").disabled = true;
+				document.getElementById("minute").value = m;
+				document.getElementById("minute").disabled = true;
+				document.getElementById("second").value = s;	
+				document.getElementById("second").disabled = true;
+				}
+
+				if(difference==0)
+				{
+					clearInterval(timer);										  /*To Stop Timer when countdown is complete*/
+				     
+					document.getElementById("hour").disabled = false;	
+					document.getElementById("minute").disabled = false;
+					document.getElementById("second").disabled = false;
+				}
+				
+				
+			},1000);
+		}
+	else if (document.getElementById("start").innerHTML=="PAUSE") 
+	{
+		document.getElementById("start").innerHTML="START"
+		difference=0;
+		document.getElementById("hour").disabled = false;
+		document.getElementById("minute").disabled = false;
+		document.getElementById("second").disabled = false;
+
+	}
+	
 }
 
-function stop()																/*Function to stop countdown and set input boxes to 0:0:0*/
+function reset()																/*Function to stop countdown and set input boxes to 0:0:0*/
 {
-		document.getElementById("hour").value = 00;
-		document.getElementById("minute").value = 00;
-		document.getElementById("second").value = 00;	
+		difference=0;
+		document.getElementById("hour").value = 00;	
+		document.getElementById("minute").value = 00;	
+		document.getElementById("second").value = 00;
+		
+		document.getElementById("hour").disabled = false;
+		document.getElementById("minute").disabled = false;
+		document.getElementById("second").disabled = false;
+
+		
 }
 
 function acceptTime()														/*Function To Receive the countdown time remaining from input boxes*/
